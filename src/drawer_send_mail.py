db2584c13f01d6d135ad204977b0e490e3fee31a
IgorAragao <igor_lamino@hotmail.com>
2025-11-16 00:19:09 -0300

    Arquivo app de envio dos nomes sorteados no email
    Criado novo arquivo python, com a lógica de enviar os nomes sorteados via email.
    
    Signed-off-by: IgorAragao <igor_lamino@hotmail.com>

---
+++ src/drawer_send_mail.py
@@ -0,0 +1,111 @@
+import random
+import smtplib
+from email.message import EmailMessage
+
+
+# variavel que acomoda os emails para envio dos nomes
+emails_destinatarios = input("Insira os emails (gmail) destinatarios separados por espaço: ").split()
+
+
+# variavel que acomoda os emails que ja foram enviados
+emails_ja_enviados = []
+
+
+# variavel que acomoda os nomes a serem sorteados
+names_input = input("Digite os nomes a serem sorteados separados por espaço: ").split()
+
+
+# variavel que acomoda os nomes que ja foram sorteados
+names_drawn = []
+
+
+def conn_mail():
+    try:
+        servidor_email = smtplib.SMTP('smtp.gmail.com', 587)
+        servidor_email.starttls()
+        servidor_email.login('<add_email>', 'add_password_app') # adicionar email gmail
+        return servidor_email
+    except Exception as err:
+        print(f"[ERROR] ❌ {err}")
+        return None
+
+
+def conn_close(conexao):
+    if conexao is None:
+        print("[ERROR] ❌ Conexão de email não estabelecida!")
+    else:
+        print("[INFO] Conexão encerrada!")
+        conexao.quit()
+
+
+def send_mail(servidor_email, dest, name):
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Amigo secreto ??/12'
+    msg['From'] = 'pydrawer@gmail.com'
+    msg['To'] = dest
+
+    msg.set_content(
+        f'Opaaaa, beleza??\n'
+        f'Esse é o e-mail com o nome do seu amigo secreto :)\n'
+        f'Para confirmar que recebeu, mande uma mensagem no WhatsApp para o Iguinho ou o Vini (Não vai falar o nome do seu amigo secreto emm hahaha).\n'
+        f'Seu amigo secreto é: {name}'
+    )
+
+    servidor_email.sendmail(msg['From'], [dest], msg.as_string())
+
+
+def drawer():
+    conexao = conn_mail()
+
+    try:
+        if conexao is not None:
+
+            while len(names_input) > 0:
+                start = input('Sortear? (Sim ou Não): ')
+
+                if start.lower() == 'sim':
+
+                    num_name = random.randrange(0, len(names_input))
+                    name = names_input[num_name]
+
+                    names_drawn.append(name)
+
+                    num_mail =  random.randrange(0, len(emails_destinatarios))
+                    email = emails_destinatarios[num_mail]
+                    send_mail(conexao, email, name)
+                    emails_ja_enviados.append(email)
+
+
+                    emails_destinatarios.remove(email)
+                    names_input.remove(name)
+
+                    print(f'Nome sorteado: {name}')
+                    print(f'Nomes já sorteados: {names_drawn}')
+                    print(f'Nomes faltantes: {names_input}')
+
+                    print(f'email enviado: {email}')
+                    print(f'email já enviado: {emails_ja_enviados}')
+                    print(f'email faltantes: {emails_destinatarios}')
+
+
+                else:
+                    print('py_drawer encerrado!')
+                    exit()
+
+        else:
+            exit()
+
+    except Exception as err:
+        print(f"[ERROR] ❌ Falha ao enviar o email: {err}")
+
+    finally:
+        conn_close(conexao)
+
+
+def main():
+    drawer()
+
+
+if __name__ == "__main__":
+    main()
